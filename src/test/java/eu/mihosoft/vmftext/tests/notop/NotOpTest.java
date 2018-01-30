d0c0f10382e92bc7712c65e26255242825973c81
Michael Hoffer <info@michaelhoffer.de>
2018-01-30 18:32:42 +0100

    Tests for not-(negate) operator '~'

--- src/test/java/eu/mihosoft/vmftext/tests/notop/NotOpTest.java
+++ src/test/java/eu/mihosoft/vmftext/tests/notop/NotOpTest.java
@@ -3,6 +3,7 @@ package eu.mihosoft.vmftext.tests.notop;
 import eu.mihosoft.vmftext.tests.notoptest.NotOpModel;
 import eu.mihosoft.vmftext.tests.notoptest.parser.NotOpModelParser;
 import eu.mihosoft.vmftext.tests.notoptest.unparser.NotOpModelUnparser;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class NotOpTest {
@@ -12,16 +13,35 @@ public class NotOpTest {
         NotOpModelParser parser = new NotOpModelParser();
         NotOpModelUnparser unparser = new NotOpModelUnparser();
 
-        NotOpModel model1 = parser.parse("abc altTwo");
-        String s1 = unparser.unparse(model1);
+        // rule 1 alt 0
+        NotOpModel model1 = parser.parse("def altOne");
+        String s1 = unparser.unparse(model1).trim();
         System.out.println("CASE1: " + s1);
 
-        NotOpModel model2 = parser.parse("def altOne");
-        String s2 = unparser.unparse(model1);
-        System.out.println("CASE2: " + s2);
+        Assert.assertEquals("def altOne", s1);
 
+        // rule 1 alt 1
+        NotOpModel model2 = parser.parse("abc altOne");
+        String s2 = unparser.unparse(model2).trim();
+        System.out.println("CASE2: " + s2);
+        Assert.assertEquals("abc altOne", s2);
+
+        // rule 2 alt 0
         NotOpModel model3 = parser.parse("abc altOne");
-        String s3 = unparser.unparse(model1);
+        String s3 = unparser.unparse(model3).trim();
         System.out.println("CASE3: " + s3);
+        Assert.assertEquals("abc altOne",s3);
+
+        // rule 2 alt 1
+        NotOpModel model4 = parser.parse("def altTwo");
+        String s4 = unparser.unparse(model4).trim();
+        System.out.println("CASE4: " + s4);
+        Assert.assertEquals("def altTwo", s4);
+
+        // rule 3 alt 0
+        NotOpModel model5 = parser.parse("this altOneR3");
+        String s5 = unparser.unparse(model5).trim();
+        System.out.println("CASE5: " + s5);
+        Assert.assertEquals("this altOneR3", s5);
     }
 }
--- test-code/NotOp.g4
+++ test-code/NotOp.g4
@@ -1,13 +1,13 @@
 grammar NotOp;
 
 
-program: r1=rule1 | r2=rule2;
+program: r1=rule1 | r2=rule2 | r3=rule3 EOF;
 
 rule1: (content=~ABC 'altOne' | content=~DEF 'altTwo');
 
-rule2: (content=~ABC 'altOne' | content=ABC 'altOne');
+rule2: content=ABC 'altOne' | content=~ABC 'altTwo' ;
 
-rule3: content=~'here';
+rule3: content2=~'notthis' 'altOneR3';
 
 ABC: [abc]+;
 
